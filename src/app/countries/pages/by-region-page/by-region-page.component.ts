d6badae1f7fa3d6af31d1ed83129da21ed12039f
san-falcon <sanfalcon200@gmail.com>
2025-02-17 16:30:55 -0500

    Busqueda de la region con nombre por defecto

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+/* interface Region { 'Africa': string; 'Americas': string; 'Asia': string, 'Europe': string, 'Oceania': string }; */
+/* enum Region {
+  Africa, Americas, Asia, Europe, Oceania
+} */
 @Component({
   selector: 'app-by-region-page',
   standalone: false,
@@ -11,12 +16,15 @@ import { CountriesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
   public data: Country[] = [];
   public isLoading: boolean = false;
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectRegion?: Region;
 
   constructor(public countriesService: CountriesService) { }
 
-  public searchByRegion(term: string): void {
+  public searchByRegion(region: Region): void {
+    this.selectRegion = region;
     this.isLoading = true;
-    this.countriesService.searchRegion(term).subscribe(resp => {
+    this.countriesService.searchRegion(region).subscribe(resp => {
       this.isLoading = false;
       this.data = resp
     });
