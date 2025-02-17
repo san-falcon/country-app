0e787d4e0d45a2e06ddc30d7ff75b1cc4f950945
san-falcon <sanfalcon200@gmail.com>
2025-02-17 12:59:04 -0500

    Mejora de las busquedas
    - Ahora en las busquedas no es necesario presionar el teclado enter, ya que despues de un determinado tiempo se lanza la peticion http

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -10,7 +11,12 @@ import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@
   `,
   standalone: false
 })
-export class SearchBoxComponent {
+
+//? Cuando el componente se inicializa por completo, se ejecuta el metodo OnInit. Se ejecuta especificamente despues del constructor
+export class SearchBoxComponent implements OnInit, OnDestroy {
+
+  public debouncer: Subject<string> = new Subject<string>();
+  public debouncerSuscription?: Subscription;
 
   @Input()
   public placeholder: string = '';
@@ -21,7 +27,35 @@ export class SearchBoxComponent {
   @Output()
   public onValue: EventEmitter<string> = new EventEmitter();
 
-  public emitValue(): void {
+  @Output()
+  public onDebounce: EventEmitter<string> = new EventEmitter();
+
+  public onKeyEnter(): void {
     this.onValue.emit(this.txtInput.nativeElement.value);
   }
+
+  public onKeyPress(searchText: string): void {
+    //? Enviamos los datos a la suscripcion ya creada
+    this.debouncer.next(searchText);
+  }
+
+  ngOnInit(): void {
+    //? Aqui creamos la suscripcion una unica vez:
+    //? Hacemos este proceso para que no me crea una suscripcion cada vez que presiono una tecla
+    this.debouncerSuscription = this.debouncer
+      .pipe(
+        debounceTime(500)
+      )
+      .subscribe(value => {
+        this.onDebounce.emit(value)
+      })
+  }
+
+  ngOnDestroy(): void {
+    //? Cuando se destruye el componente se ejecuta metodo
+    //? Para eliminar la instancia de mi Observable y no quede en memoria
+    //? Se elimnina porque cada vez que utilizamos el componente shared-search-box Angular crea una instancia de mi suscribe
+    this.debouncerSuscription?.unsubscribe();
+  }
+
 }
